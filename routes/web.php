6191f5e049c6c9352b38f09da11a41a1d656dea9
RykaBluud <101121099+RykaBluud@users.noreply.github.com>
2023-03-05 05:23:14 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/machines', 'SearchController@index');
+Route::get('/machines/simple', 'SearchController@simple')->name('simple_search');
+Route::get('/machines/advance', 'SearchController@advance')->name('advance_search');
